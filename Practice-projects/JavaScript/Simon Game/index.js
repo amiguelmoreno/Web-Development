0d74737c2e55fe2a83fce314e6f333ab23815e3c
Antonio Miguel Moreno Molina <>
2022-09-28 02:38:16 +0200

    finishing simon game, only some features left

--- Practice-projects/JavaScript/Simon Game/index.js
+++ Practice-projects/JavaScript/Simon Game/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//selecting colors buttons and audios
+/* //selecting colors buttons and audios
 const yellowEl = document.querySelector(".yellow-box");
 const yellowAudio = new Audio("./audios/simonSound1.mp3");
 const blueEl = document.querySelector(".blue-box");
@@ -10,54 +10,128 @@ const redAudio = new Audio("./audios/simonSound3.mp3");
 const greenEl = document.querySelector(".green-box");
 const greenAudio = new Audio("./audios/simonSound4.mp3");
 const errorAudio = new Audio("./audios/errorSound.mp3");
-const startBtn = document.querySelector(".start");
-const resetBtn = document.querySelector(".reset");
+
 const audiosEl = document.querySelectorAll(".clip");
-const scoreValueEl = document.querySelector(".score-value");
+
 const bestScoreValueEl = document.querySelector(".best-score-value");
 
 const colorsArr = [yellowEl, blueEl, redEl, greenEl];
 const audioArr = [yellowAudio, blueAudio, redAudio, greenAudio];
-const colorsAnimation = ["yellow", "blue", "red", "green"];
-const colorsOriginal = ["#fdff8bcc", "#8f8fffcc", "#e88585cc", "#559555cc"];
 
-let lightsOrder = [];
-let playerLightsOrder = [];
-let flashNumber;
-let turn;
-let good;
-let compTurn;
-let intervalId;
+const colorsOriginal = ["#fdff8bcc", "#8f8fffcc", "#e88585cc", "#559555cc"]; */
 
-startBtn.addEventListener("click", play);
+const startBtn = document.querySelector(".start");
+const resetBtn = document.querySelector(".reset");
+const scoreValueEl = document.querySelector(".score-value");
+const soundError = new Audio(`./audios/errorSound.mp3`);
+const tilesContainer = document.querySelector(".tiles-container");
+const colors = ["yellow", "blue", "red", "green"];
 
-function play() {
-    resetGame();
-    startBtn.removeEventListener("click", play);
+let sequence = [];
+let humanSequence = [];
+let level = 0;
 
-    compGame();
+function resetGame(text) {
+    alert(text);
+    sequence = [];
+    humanSequence = [];
+    level = 0;
+    startBtn.classList.remove("hidden");
+    scoreValueEl.textContent = level;
+    tilesContainer.classList.add("unclickable");
 }
 
-function resetGame() {
-    lightsOrder = [];
-    playerLightsOrder = [];
-    flashNumber = 0;
-    intervalId = 0;
-    turn = 1;
-    scoreValueEl.innerHTML = turn;
-    compTurn = true;
+//allow to click colors and change score
+function humanTurn(level) {
+    tilesContainer.classList.remove("unclickable");
+    scoreValueEl.textContent = level;
 }
 
-function compGame() {
-    const timeBtwColors = 500;
-    //lightsOrder.push(Math.floor(Math.random() * 4) + 1);
-    lightsOrder = [3, 1, 0, 2, 3, 0, 2];
-    lightsOrder.forEach(function (color, i) {
-        setTimeout(function () {
-            colorsArr[el].style.backgroundColor = colorsAnimation[el];
-            audioArr[el].play();
-        }, i * timeBtwColors);
+//activate the button color and sound
+function activateButton(color) {
+    const button = document.querySelector(`#${color}`);
+    const sound = new Audio(
+        `./audios/simonSound${colors.indexOf(color) + 1}.mp3`
+    );
+
+    button.classList.add("activated");
+    sound.play();
+
+    setTimeout(() => {
+        button.classList.remove("activated");
+    }, 300);
+}
+
+//run over the sequence and apply the function to each button
+function playRound(nextSequence) {
+    nextSequence.forEach((color, index) => {
+        setTimeout(() => {
+            activateButton(color);
+        }, (index + 1) * 600);
     });
 }
 
-function btnAnimation(el) {}
+//generate random color
+function nextStep() {
+    const random = colors[Math.floor(Math.random() * colors.length)];
+
+    return random;
+}
+
+function nextRound() {
+    level += 1;
+
+    //disable buttons and updating score
+    tilesContainer.classList.add("unclickable");
+    scoreValueEl.textContent = level;
+
+    //copy the old sequence and adding the new random color
+    const nextSequence = [...sequence];
+    nextSequence.push(nextStep());
+    playRound(nextSequence);
+
+    //adding delay to notice the turn change and updating sequence
+    sequence = [...nextSequence];
+    setTimeout(() => {
+        humanTurn(level);
+    }, level * 600 + 1000);
+}
+
+//Disable the start button when clicked
+startBtn.addEventListener("click", startGame);
+tilesContainer.addEventListener("click", (event) => {
+    const tile = event.target.id;
+    event.target.classList.add("activated");
+
+    setTimeout(() => {
+        event.target.classList.remove("activated");
+    }, 300);
+    if (tile) handleClick(tile);
+});
+
+function handleClick(tile) {
+    const index = humanSequence.push(tile) - 1;
+    const sound = new Audio(
+        `./audios/simonSound${colors.indexOf(tile) + 1}.mp3`
+    );
+    sound.play();
+
+    if (humanSequence[index] !== sequence[index]) {
+        soundError.play();
+        resetGame("Oops! Game over, you pressed the wrong tile");
+        return;
+    }
+
+    if (humanSequence.length === sequence.length) {
+        humanSequence = [];
+        setTimeout(() => {
+            nextRound();
+        }, 1000);
+        return;
+    }
+}
+
+function startGame() {
+    startBtn.classList.add("hidden");
+    nextRound();
+}
