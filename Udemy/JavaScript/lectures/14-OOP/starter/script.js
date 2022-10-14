e4a050c2b71abb136a29cad2a2f16a909bc41d27
Miguel Moreno <amiguelmoreno42@gmail.com>
2022-10-14 14:11:55 +0200

    practicing section 15 js

--- Udemy/JavaScript/lectures/14-OOP/starter/script.js
+++ Udemy/JavaScript/lectures/14-OOP/starter/script.js
@@ -67,6 +67,7 @@ class Car2 {
   brake() {
     this.speed -= 5;
     console.log(this.speed);
+    return this;
   }
 
   get speedUS() {
@@ -92,7 +93,7 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-const EV = function (make, speed, charge) {
+/* const EV = function (make, speed, charge) {
   Car.call(this, make, speed);
   this.charge = charge;
 };
@@ -122,3 +123,36 @@ tesla.accelerate();
 tesla.accelerate();
 tesla.brake();
 tesla.chargeBattery(75);
+ */
+
+// Coding Challenge #
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class EVCl extends Car2 {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(this.speed, this.#charge);
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian.accelerate().accelerate().brake();
