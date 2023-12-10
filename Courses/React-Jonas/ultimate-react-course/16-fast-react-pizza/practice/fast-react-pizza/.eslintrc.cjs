43734678f67f546a71469e31507e8c52d2703341
Miguel Moreno <amiguelmoreno42@gmail.com>
2023-12-10 20:25:53 +0100

    start tailwind section, set up and more

---
+++ Courses/React-Jonas/ultimate-react-course/16-fast-react-pizza/practice/fast-react-pizza/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/prop-types": "off", // Desactiva la validación de props
+    "no-unused-vars": "off",
+  },
+};
---
+++ Courses/React-Jonas/ultimate-react-course/16-fast-react-pizza/practice/fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+  return (
+    <div className="hidden text-sm font-semibold md:block md:text-3xl">
+      Miguel
+    </div>
+  );
+}
+
+export default Username;
