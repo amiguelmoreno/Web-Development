6df7503e01ec67c17db4959c428c0bc85c845bf2
Miguel Moreno Molina <99278397+amiguelmoreno@users.noreply.github.com>
2024-04-30 17:59:55 +0200

    express course

--- Courses/React/React-Jonas/ultimate-react-course/04-steps/final/src/App.js
+++ Courses/React/React-Jonas/ultimate-react-course/04-steps/final/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "mycomponentsui";
 
 const messages = [
   "Learn React ⚛️",
@@ -46,13 +47,13 @@ function Steps() {
 
   return (
     <div>
-      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+      <button className='close' onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
 
       {isOpen && (
-        <div className="steps">
-          <div className="numbers">
+        <div className='steps'>
+          <div className='numbers'>
             <div className={step >= 1 ? "active" : ""}>1</div>
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
@@ -60,10 +61,10 @@ function Steps() {
 
           <StepMessage step={step}>
             {messages[step - 1]}
-            <div className="buttons">
+            <div className='buttons'>
               <Button
-                bgColor="#e7e7e7"
-                textColor="#333"
+                bgColor='#e7e7e7'
+                textColor='#333'
                 onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
               >
                 Learn how
@@ -71,12 +72,12 @@ function Steps() {
             </div>
           </StepMessage>
 
-          <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+          <div className='buttons'>
+            <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}>
               <span>👈</span> Previous
             </Button>
 
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+            <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext}>
               Next <span>👉</span>
               <span>🤓</span>
             </Button>
@@ -89,14 +90,14 @@ function Steps() {
 
 function StepMessage({ step, children }) {
   return (
-    <div className="message">
+    <div className='message'>
       <h3>Step {step}</h3>
       {children}
     </div>
   );
 }
 
-function Button({ textColor, bgColor, onClick, children }) {
+/* function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
@@ -105,4 +106,4 @@ function Button({ textColor, bgColor, onClick, children }) {
       {children}
     </button>
   );
-}
+} */
